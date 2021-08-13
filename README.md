dac229f6db3b40aec9093159beb7745bf5c8a7b2
Tom White <tomwhite007@gmail.com>
2021-08-13 08:58:05 +0100

    added test function

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Node Dev Environment Seed
 
 Code and webpack config lifted from VS Code Extension dev environment structure
+
+Currently unit tests don't work, but build and watch does.
--- src/code.ts
+++ src/code.ts
@@ -1,5 +1,33 @@
-console.log("hello world - yeah", getText());
+const name = ["ball", "bat", "glove", "glove", "glove"];
+const price = [2, 3, 1, 2, 1];
+const weight = [2, 5, 1, 1, 1];
+const dups = numDuplicates(name, price, weight);
+console.log("number of duplicates", dups);
 
-function getText() {
-  return "text";
+function numDuplicates(name: string[], price: number[], weight: number[]) {
+  if (name.length > 1000000) {
+    throw new Error("Constraint: greater than 10 to the 5");
+  }
+
+  const stringifiedArray: string[] = [];
+  for (let i = 0; i < name.length; i++) {
+    if (!name[i].match(/^[a-z/]{1,10}$/)) {
+      throw new Error("Constraint: name text is not the right size or chars");
+    }
+    if (price[i] < 1) {
+      throw new Error("Constraint: price is too small");
+    }
+    if (weight[i] > 1000) {
+      throw new Error("Constraint: weight is too heavy");
+    }
+    stringifiedArray.push(`${name[i]}, ${price[i]}, ${weight[i]}`);
+  }
+  console.log("merged", stringifiedArray);
+
+  const duplicates = stringifiedArray.filter(
+    (item, index) => stringifiedArray.indexOf(item) !== index
+  );
+  console.log("duplicates", duplicates);
+
+  return duplicates.length;
 }
